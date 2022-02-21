cec374b24181bcde7a0e7387329f3507f49d9020
Oleg Klymchuk <koi196621@gmail.com>
2022-02-21 10:52:42 +0200

    Примеры использования стрима и лямды, мапы 2.02.2022

--- src/main/java/MapIndString.java
+++ src/main/java/MapIndString.java
@@ -10,21 +10,54 @@ public class MapIndString {
 //                          2   1     1    2      2   2     2    1    1      2
         String stString ="Ваня Ваня Петя Саша Максим Маша Ваня Маша Вова Андрей Максим";
         String[] str = stString.split(" ");
-        Integer Imap = 1;
-        Integer Jmap =1;
-
+        int Imap = 1;
         HashMap<String,Integer> heshMapp = new HashMap<>();
-        for (int i = 0; i < str.length; i++) {
-            heshMapp.put(str[i],Imap);
-            for (int j = 0; j < str.length; j++) {
+        for (String s : str) {
+            heshMapp.put(s, Imap);
+            for (String value : str) {
 
-               if (str[i].equals(str[j])){
-                   heshMapp.put(str[i],Imap++);
-               }
+                if (s.equals(value)) {
+                    heshMapp.put(s, Imap++);
+                }
             }
-            Imap =1;
+            Imap = 1;
         }
-        System.out.println(heshMapp.toString());
+
+//        for (int i = 0; i < str.length; i++) {
+//            heshMapp.put(str[i],Imap);
+//            for (int j = 0; j < str.length; j++) {
+//
+//               if (str[i].equals(str[j])){
+//                   heshMapp.put(str[i],Imap++);
+//               }
+//            }
+//            Imap =1;
+//        }
+//        System.out.println(heshMapp.toString());
+        System.out.println(heshMapp);
+
+        System.out.println("=======================================");
+//        HashMap<String,Integer> heshMapp2 = new HashMap<>();
+//        for (int i = 0; i < str.length; i++) {
+//            if (heshMapp2.containsKey(str[i])) {
+//                Integer temp = heshMapp2.get(str[i]);
+//                heshMapp2.put(str[i],++temp);
+//            } else
+//                heshMapp2.put(str[i],1);
+//        }
+
+        HashMap<String,Integer> heshMapp2 = new HashMap<>();
+        for (String s : str) {
+            if (heshMapp2.containsKey(s)) {
+                Integer temp = heshMapp2.get(s);
+                heshMapp2.put(s, ++temp);
+            } else
+                heshMapp2.put(s, 1);
+        }
+//        System.out.println(heshMapp2.toString());
+        System.out.println(heshMapp2);
+
+        System.out.println("=======================================");
 
         List<String> listF = new ArrayList<>();
         listF.add("White");
@@ -37,7 +70,7 @@ public class MapIndString {
                 if (listF.get(i).equals(listF.get(j)) && (i != j)) count++;
 
             }
-            System.out.println(listF.toString());
+            System.out.println(listF);
         }
     }
 }
