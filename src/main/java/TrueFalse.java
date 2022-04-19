c0be49aceb07a54c3cd39f6ee15ba59925d1151e
Oleg Klymchuk <koi196621@gmail.com>
2022-04-19 08:00:03 +0300

    Добавлен абстракный класс, Интерфейсы в DOC

--- src/main/java/TrueFalse.java
+++ src/main/java/TrueFalse.java
@@ -7,5 +7,12 @@ public class TrueFalse {
         } else {
             System.out.println("false");
         }
+
+        int ar = null;
+        int[] ar1 = new int[-5];
+        int[] ar2 = new int['a'];
+//        int[] ar3 = new int[2147483648];
+        int num1 = 0xFace   ;
+
     }
 }
