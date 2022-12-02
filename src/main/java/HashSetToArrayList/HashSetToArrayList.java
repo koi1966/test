5273fd77eec2124e755c5668adb5bebc41ffc9a6
Oleg Klimchuk <koi196621@gmail.com>
2022-12-02 13:59:51 +0200

    22.11.2022

---
+++ src/main/java/HashSetToArrayList/HashSetToArrayList.java
@@ -0,0 +1,13 @@
+package HashSetToArrayList;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+//Как одной строчкой преобразовать HashSet в ArrayList?
+public class HashSetToArrayList {
+
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>(new ArrayList<>());
+
+    }
+}
