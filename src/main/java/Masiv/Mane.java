47adc16958a3da84c253f4d39df101cf00ed7baf
Oleg Klimchuk <koi196621@gmail.com>
2023-09-09 14:19:27 +0300

    19 07 2023

---
+++ src/main/java/Masiv/Mane.java
@@ -0,0 +1,21 @@
+package Masiv;
+
+public class Mane {
+    public static void main(String[] args) {
+//         int[] array = new int[3];
+         int [] array = new int[]{1,0,0};
+
+//        int [] array = {1,0,0}
+//        array[0]= 1;
+//        array[1]= 0;
+//        array[2]= 0;
+
+
+        Solution solution = new Solution(array);
+
+
+        solution.plusOne( array);
+    }
+
+}
+
