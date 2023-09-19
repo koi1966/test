98103eb4b1d6f89c1d14f14f7f7cb69e84e6d650
Oleg Klimchuk <koi196621@gmail.com>
2023-09-19 12:56:12 +0300

    19 07 2023

--- src/main/java/String1/main.java
+++ src/main/java/String1/main.java
@@ -6,5 +6,13 @@ public class main {
         StringInt stringInt = new StringInt();
         System.out.println(stringInt.calculateFromString("2+3*2"));
 
+
+        System.out.println("================");
+        int a = 10;
+        int b = 5;
+        System.out.println(a + ' ' + b);
+        System.out.println(a + ' ' );
+        System.out.println( ' ' + b);
+
     }
 }
